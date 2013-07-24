114f531e3e57ac10c1d9b4f0f3622a398cf59a75
Clark Everetts <clark.everetts@gmail.com>
2013-07-24 06:33:12 -0500

    ZF2/ServiceManager: Add clarifications and a few more examples to introduction.php

--- ZF2/README.md
+++ ZF2/README.md
@@ -1,4 +1,4 @@
 # Code Examples for Zend Framework 2
 
-First, we'll deal with examples for the pillars of ZF2; namely, EventManager, ServiceManager, and ModuleManager. Then, we'll have examples for the MVC Module, which is a Module that is really just a collection of default events and services to use to build MVC applications. Also to follow will be examples of Modules and other ZF2 components.
+First, we'll deal with examples for the pillars of ZF2; namely, EventManager, ServiceManager, and ModuleManager. Then, we'll have examples for the MVC Module, which is a Module that is really just a collection of default events and services to use to build MVC applications. Also to follow will be examples of Modules, ZF2 components, and MVC applications.
 
--- ZF2/ServiceManager/introduction.php
+++ ZF2/ServiceManager/introduction.php
@@ -2,12 +2,18 @@
 include __DIR__ . '/' . 'includes.php';
 use Zend\ServiceManager;
 
-$arrayService = array('This service happens to be an array.');
+$arrayService = array('This service happens to be an array. Services are intended to be objects, however.');
+
+// This class is the template for a simple service
+class SimpleClass
+{
+    public $attribute = 'SimpleClassAttribute';
+}
 
 echo 'Instantiate SM without configuration', PHP_EOL;
 $sm = new ServiceManager\ServiceManager();
 
-echo 'What are all the services the SM is currently aware of?', PHP_EOL;
+echo 'What are all the services the SM is currently aware of? Use getRegisteredServices().', PHP_EOL;
 echo print_r($sm->getRegisteredServices(), true), PHP_EOL;
 
 echo 'Register a service with the SM', PHP_EOL;
@@ -20,81 +26,86 @@ echo print_r($service, true), PHP_EOL;
 echo 'Destroy original resource', PHP_EOL;
 unset($arrayService);
 
-echo 'Retrieve the service again; it is still available', PHP_EOL;
+echo 'Retrieve the service again; it is still available (SM using references?)', PHP_EOL;
 $service = $sm->get('some-service');
 echo print_r($service, true), PHP_EOL;
 
 try {
-    echo 'Attempt to register a service with the SM with a name that is already in use', PHP_EOL;
+    echo 'Attempt to register a service with a name that is already in use', PHP_EOL;
     $objectService = new StdClass();
     $sm->setService('some-service', $objectService);
 } catch (ServiceManager\Exception\InvalidServiceNameException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 
-echo 'We can still get the service we previously registered', PHP_EOL;
+echo 'Of course, we can still retrieve the service we previously registered', PHP_EOL;
 $service = $sm->get('some-service');
 echo print_r($service, true), PHP_EOL;
 
-echo 'Show all registered services', PHP_EOL;
+echo 'Show all registered services. Note that getRegisteredServices() returns canonicalized names (lowercased, with word separator characters removed).', PHP_EOL;
 echo print_r($sm->getRegisteredServices(), true), PHP_EOL;
 
-echo 'Are exceptions thrown in SM create method? - ';
+echo 'Show all canonical service names by calling getCanonicalNames().', PHP_EOL;
+echo print_r($sm->getCanonicalNames(), true), PHP_EOL;
+
+echo 'Are exceptions thrown in SM create() method? - ';
 var_dump($sm->getThrowExceptionInCreate()); echo PHP_EOL;
 
 try {
+    echo 'The create() method returns only object instances', PHP_EOL;
     $service = $sm->create('some-service');
 } catch (ServiceManager\Exception\ServiceNotFoundException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 
-echo 'Register object instance service with the SM and show all registered services', PHP_EOL;
+echo 'Can check to see if the SM can create the service by calling canCreate() method, but note that only checks for the existence of the service name, not whether it can instantiate the service. Also note that you can always call has() instead of canCreate(), since has() itself calls canCreate() !', PHP_EOL
+   , 'canCreate(\'some-service\') returns ';
+var_dump($sm->canCreate('some-service'));
+echo PHP_EOL;
+
+echo 'Register an object instance with the SM and show all registered services', PHP_EOL;
 try {
     $sm->setService('object-service', $objectService);
 } catch (ServiceManager\Exception\InvalidServiceNameException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 } catch (ServiceManager\Exception\ServiceNotFoundException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 echo 'Line ', __LINE__, '. After setting object service', print_r($sm->getRegisteredServices(), true), PHP_EOL;
 
-echo 'SM create method is for getting an object from a factory, invokable class, an abstract factory, or an initializer, not for an object instance already registered. Attempt to create service using name of object service', PHP_EOL;
+echo 'SM create() method is for creating an object instance from a factory, invokable class, abstract factory, or an initializer. create() is not for retrieving an object instance already registered. An attempt to create a service using the service name of object already registered yields an exception:', PHP_EOL;
 try {
     $service = $sm->create('object-service');
 } catch (ServiceManager\Exception\InvalidServiceNameException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 } catch (ServiceManager\Exception\ServiceNotFoundException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 
-echo 'Using get method, retrieve the object service that was successfully registered', PHP_EOL;
+echo 'Using get() method, the previously-registered object can be retrieved.', PHP_EOL;
 $service = $sm->get('object-service');
 var_dump($service); echo PHP_EOL;
 
-echo 'Register and attempt to create service using name of a class', PHP_EOL;
-class SimpleClass
-{
-    public $attribute = 'SimpleClassAttribute';
-    public function __construct()
-    {
-    }
-}
+echo 'Use setInvokableClass() to register a service using the name of a class. Then use create() method to retrieve an instance of that class.', PHP_EOL;
+
 try {
     $sm->setInvokableClass('invokable-class', 'SimpleClass');
-    echo 'Line ', __LINE__, 'service successfully set using class name: ', print_r($sm->getRegisteredServices(), true), PHP_EOL;
+    echo 'Line ', __LINE__, ': service successfully set using class name: ', PHP_EOL, print_r($sm->getRegisteredServices(), true), PHP_EOL;
+    echo 'Can use the SM has() method to check for existence of a service by name: ';
+    var_dump($sm->has('invokable-class'));
+    echo PHP_EOL;
 } catch (ServiceManager\Exception\InvalidServiceNameException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 } catch (ServiceManager\Exception\ServiceNotFoundException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 
 try {
-    echo 'Attempt to retrieve the service we just set', PHP_EOL;
+    echo 'Retrieve the service we just registered', PHP_EOL;
     $service = $sm->get('invokable-class');
-    var_dump($service); echo PHP_EOL;
-} catch (ServiceManager\Exception\InvalidServiceNameException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    var_dump($service);
+    echo PHP_EOL;
 } catch (ServiceManager\Exception\ServiceNotFoundException $e) {
-    echo 'Line ', __LINE__, ': ', $e->getMessage(), PHP_EOL;
+    echo 'Line ', __LINE__, ': ', get_class($e), ' - ', $e->getMessage(), PHP_EOL, PHP_EOL;
 }
 
